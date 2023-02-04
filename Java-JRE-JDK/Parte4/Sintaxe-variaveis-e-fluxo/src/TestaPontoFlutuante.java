2bdd66e4db5bfd636dc2adb8e7e8fc48acdbe740
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-02-03 21:41:05 -0300

    concluido o modulo 4 com 12 videos e exercicios

---
+++ Java-JRE-JDK/Parte4/Sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,23 @@
+
+public class TestaPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double salario = 1250.70;
+		System.out.println("meu salario é " + salario);
+		
+		double idade = 31;
+		
+		double divisao = 3.14 / 2;
+		System.out.println(divisao);
+		
+		int outraDivisao = 5 / 2;
+		System.out.println(outraDivisao);
+		
+		double novaTentativa = 5.0 / 2;
+		System.out.println(novaTentativa);
+		
+		
+	}
+	
+}
---
+++ Java-JRE-JDK/Parte4/Sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,25 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("ola novo teste");
+		
+		int idade;
+		idade = 31;
+		
+		System.out.println(idade);
+		
+		idade =30 + 10;
+		
+		idade = (7 * 5) +2;
+		
+		System.out.println(idade);
+		
+		System.out.println("a idade é " + idade + ", parabéns!");
+		
+		System.out.print(idade);
+		
+		
+		
+	}
+}
