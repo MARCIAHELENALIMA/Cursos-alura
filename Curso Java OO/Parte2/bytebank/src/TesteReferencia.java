50d258a79bea9da80b19665cdafdb190d239affc
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-02-05 11:47:14 -0300

    Concluido os 12 passos deste modulo

---
+++ Curso Java OO/Parte2/bytebank/src/TesteReferencia.java
@@ -0,0 +1,10 @@
+public class TesteReferencia { 
+    public static void main(String[] args) {
+        Conta primeiraConta = new Conta(); 
+        primeiraConta.saldo = 300;
+
+        System.out.println("saldo da primeira: " + primeiraConta.saldo);
+
+        Conta segundaConta = primeiraConta;
+    }
+}
