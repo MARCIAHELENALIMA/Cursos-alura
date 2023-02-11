f4e6385b34f63563950875350f7b2fcea2eba6bb
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-02-11 15:41:25 -0300

    Conclui os 10 passos deste modulo Pilha de execução

---
+++ Curso Java Exceções/1- Pilha de execução/javaPilha/src/Principal.java
@@ -0,0 +1,22 @@
+
+
+	public class Principal {
+
+	    public static void main(String[] args) {
+	        System.out.println("MAIN BEGIN");
+	        m1();
+	        System.out.println("MAIN END");
+	    }
+
+	    public static void m1() {
+	        System.out.println("B BEGIN");
+	        m2();
+	        System.out.println("B END");
+	    }
+
+	    public static void m2() {
+	        System.out.println("A BEGIN");
+	        System.out.println("A END");
+	    }
+	}
+
