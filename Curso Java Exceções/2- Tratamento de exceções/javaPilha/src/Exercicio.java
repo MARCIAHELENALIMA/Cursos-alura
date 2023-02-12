fe8617cb762b7bb4eac548336ca494bab418e3a5
MARCIAHELENALIMA <marciahelenalimac@gmail.com>
2023-02-12 11:29:15 -0300

    Conclui os 10 passos deste modulo Tratamento de exceções

---
+++ Curso Java Exceções/2- Tratamento de exceções/javaPilha/src/Exercicio.java
@@ -0,0 +1,19 @@
+
+public class Exercicio {
+
+	//FORMA DE ADICIONAR EXTENSA E NAO UTILIZADA PARA RESOLVER MAIS DE UMA EXCEPTION
+	try {
+	    System.out.println(1/0);
+	} catch(ArithmeticException ex) {
+	    ex.printStackTrace();
+	} catch(NullPointerException ex) {
+	    ex.printStackTrace();    
+	}
+	
+	//FORMA RESUMIDA DE ADICIONAR MAIS DE UMA EXCEPTION
+	try {
+	    System.out.println(1 / 0);
+	} catch (ArithmeticException | NullPointerException ex) {
+	    ex.printStackTrace();
+	}
+}
