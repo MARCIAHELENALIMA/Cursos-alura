ea9601ee587a0160caf1fbcd536c534d2fd8d234
Márcia Helena <marciahelenalimac@gmail.com>
2023-06-28 19:54:47 -0300

    atualização do curso

---
+++ Curso Java JRE-JDK 8h/5- Trabalhando com caracteres/sintaxe-basica/src/Programa.java
@@ -0,0 +1,12 @@
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("ola mundo");
+	}
+}
+
+
+String saudacao = "Olá, meu nome é ";
+String nome = "Rômulo ";
+String continuacao = "e minha idade é ";
+int idade = 100;
+System.out.println(saudacao +nome +continuacao +idade); 
